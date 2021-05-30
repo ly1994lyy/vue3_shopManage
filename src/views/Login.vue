d7b6f64249468f2e577793aea03667b42bb20764
ly1994lyy <1141730852@qq.com>
2021-05-30 14:33:08 +0800

    feat: 框架搭建&&登录页

---
+++ src/views/Login.vue
@@ -0,0 +1,13 @@
+<template>
+$END$
+</template>
+
+<script>
+export default {
+name: "Login"
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/views/Welcome.vue
@@ -0,0 +1,13 @@
+<template>
+$END$
+</template>
+
+<script>
+export default {
+name: "Welcome"
+}
+</script>
+
+<style scoped>
+
+</style>
